82348c4109d78db324cb7fd260ced2f0b1702c02
Pendax <frederik-lai@outlook.com>
2019-11-21 13:51:43 +0100

    Initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Fordel = new System.Windows.Forms.Button();
+            this.Navne = new System.Windows.Forms.ComboBox();
+            this.Glemt = new System.Windows.Forms.Button();
+            this.MANGLER = new System.Windows.Forms.Button();
+            this.genuddel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Fordel
+            // 
+            this.Fordel.BackColor = System.Drawing.Color.Transparent;
+            this.Fordel.Location = new System.Drawing.Point(11, 4);
+            this.Fordel.Name = "Fordel";
+            this.Fordel.Size = new System.Drawing.Size(118, 75);
+            this.Fordel.TabIndex = 0;
+            this.Fordel.Text = "Fordel nisser";
+            this.Fordel.UseVisualStyleBackColor = false;
+            this.Fordel.Click += new System.EventHandler(this.Fordel_Click);
+            // 
+            // Navne
+            // 
+            this.Navne.FormattingEnabled = true;
+            this.Navne.Items.AddRange(new object[] {
+            "Andreas",
+            "Arne",
+            "Frederik",
+            "Henning",
+            "Jannie",
+            "Karoline",
+            "Kristian",
+            "Lise",
+            "Maj",
+            "Mikkel",
+            "Mogens ",
+            "Niklas",
+            "Per",
+            "Rikke",
+            "Simon"});
+            this.Navne.Location = new System.Drawing.Point(12, 4);
+            this.Navne.Name = "Navne";
+            this.Navne.Size = new System.Drawing.Size(116, 21);
+            this.Navne.Sorted = true;
+            this.Navne.TabIndex = 1;
+            this.Navne.Visible = false;
+            // 
+            // Glemt
+            // 
+            this.Glemt.BackColor = System.Drawing.Color.Transparent;
+            this.Glemt.Location = new System.Drawing.Point(11, 31);
+            this.Glemt.Name = "Glemt";
+            this.Glemt.Size = new System.Drawing.Size(118, 29);
+            this.Glemt.TabIndex = 2;
+            this.Glemt.Text = "Send nisse\r\n";
+            this.Glemt.UseVisualStyleBackColor = false;
+            this.Glemt.Visible = false;
+            this.Glemt.Click += new System.EventHandler(this.GlemtNisse_Click);
+            // 
+            // MANGLER
+            // 
+            this.MANGLER.BackColor = System.Drawing.Color.Transparent;
+            this.MANGLER.Location = new System.Drawing.Point(11, 66);
+            this.MANGLER.Name = "MANGLER";
+            this.MANGLER.Size = new System.Drawing.Size(118, 29);
+            this.MANGLER.TabIndex = 4;
+            this.MANGLER.Text = "Glemt Ønskeliste";
+            this.MANGLER.UseVisualStyleBackColor = false;
+            this.MANGLER.Visible = false;
+            this.MANGLER.Click += new System.EventHandler(this.MANGLER_Click);
+            // 
+            // genuddel
+            // 
+            this.genuddel.BackColor = System.Drawing.Color.Transparent;
+            this.genuddel.Location = new System.Drawing.Point(11, 101);
+            this.genuddel.Name = "genuddel";
+            this.genuddel.Size = new System.Drawing.Size(118, 29);
+            this.genuddel.TabIndex = 5;
+            this.genuddel.Text = "Genfordel nisser";
+            this.genuddel.UseVisualStyleBackColor = false;
+            this.genuddel.Visible = false;
+            this.genuddel.Click += new System.EventHandler(this.genuddel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(140, 173);
+            this.Controls.Add(this.genuddel);
+            this.Controls.Add(this.MANGLER);
+            this.Controls.Add(this.Fordel);
+            this.Controls.Add(this.Glemt);
+            this.Controls.Add(this.Navne);
+            this.MinimumSize = new System.Drawing.Size(156, 39);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Nisser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Fordel;
+        private System.Windows.Forms.ComboBox Navne;
+        private System.Windows.Forms.Button Glemt;
+        private System.Windows.Forms.Button MANGLER;
+        private System.Windows.Forms.Button genuddel;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Net.Mail;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static Random _r = new Random();
+
+        public static void sendEmail(string giftGiver, string message, string GiftReciever)
+        {
+            string email;
+            switch (giftGiver)
+            {
+                case "Henning":
+                    email = "Henninglai@gmail.com";
+                    break;
+                case "Rikke":
+                    email = "Aril@aarhus.dk";
+                    break;
+                case "Frederik":
+                    email = "Frederik-lai@outlook.com";
+                    break;
+                //case "Andreas":
+                //    email = "";
+                //    break;
+                case "Karoline":
+                    email = "karolinela1903@hotmail.com";
+                    break;
+                //case "Maiken":
+                //    email = "";
+                //    break;
+                case "Mogens":
+                    email = "mo.holm@live.dk";
+                    break;
+                case "Lise":
+                    email = "Woobylulle@hotmail.com";
+                    break;
+                case "Simon":
+                    email = "Bassenoir@hotmail.com";
+                    break;
+                case "Kristian":
+                    email = "Kristian_lyngby@hotmail.com";
+                    break;
+                case "Janni":
+                    email = "Janni210294@gmail.com";
+                    break;
+                case "Per":
+                    email = "pma@aaservice.dk";
+                    break;
+                case "Maj":
+                    email = "Missionmaj@yahoo.dk";
+                    break;
+                case "Mikkel":
+                    email = "Meldgaardmikkel@gmail.com";
+                    break;
+                case "Niklas":
+                    email = "Meldgaard0409@gmail.com";
+                    break;
+                case "Michelle":
+                    email = "Michelle2010@live.dk";
+                    break;
+                case "Arne":
+                    email = "aa3812@live.dk";
+                    break;
+                default:
+                    email = "";
+                    break;
+            }
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress("hjolleren@gmail.com", "Julemanden");
+                //mail.To.Add("frederiklaiandersen@gmail.com");
+                //mail.To.Add(email);
+                mail.Subject = "Secret Santa";
+                mail.Body = "Hej " + giftGiver + message + GiftReciever;
+
+                SmtpServer.Port = 587;
+                SmtpServer.UseDefaultCredentials = false;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("hjolleren@gmail.com", "cafe1122");
+                SmtpServer.EnableSsl = true;
+
+                //SmtpServer.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                MessageBox.Show("Der skete en fejl");
+            }
+            MessageBox.Show(giftGiver + " Har fået tildelt" + GiftReciever);
+        }
+
+        public static void secretSanta(string[] input)
+        {
+            List<string> giftRecepients = new List<string>();
+            List<string> everybody = new List<string>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                var matches = Regex.Matches(input[i], @"\w+");
+                for (int j = 0; j < matches.Count; j++)
+                {
+                    everybody.Add(matches[j].Value);
+                    int randomLine, randomName, index;
+                    string recepient;
+                    MatchCollection lineMatches;
+                    do
+                    {
+                        do
+                        {
+                            randomLine = _r.Next(input.Length);
+                        } while (randomLine == i);
+                        lineMatches = Regex.Matches(input[randomLine], @"\w+");
+                        randomName = _r.Next(lineMatches.Count);
+                        recepient = lineMatches[randomName].Value;
+                        index = everybody.IndexOf(recepient);
+                    } while (giftRecepients.Contains(recepient) || (index % 2 == 0 && everybody[index + 1] == matches[j].Value));
+                    giftRecepients.Add(recepient);
+                    everybody.Add(recepient);
+                    Properties.Settings.Default[matches[j].Value] = recepient;
+                    Properties.Settings.Default.Save();
+                    sendEmail(matches[j].Value, " i år er du nisse for ", recepient);
+                    
+                }
+            }
+        }
+
+        private void GlemtNisse_Click(object sender, EventArgs e)
+        {
+            if (Navne.SelectedItem == null)
+            {
+                MessageBox.Show("Du skal vælge en person");
+            }
+            else
+            {
+               
+            }
+        }
+
+        private void Fordel_Click(object sender, EventArgs e)
+        {
+            string[] instructions = File.ReadAllLines(@"C:\Users\Frederik\secretsanta.txt");
+            secretSanta(instructions);
+            Properties.Settings.Default.Uddelt = true;
+            Properties.Settings.Default.Save();
+            Glemt.Visible = true;
+            MANGLER.Visible = true;
+            genuddel.Visible = true;
+            Navne.Visible = true;
+            Fordel.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.Uddelt)
+            {
+                Glemt.Visible = true; 
+                MANGLER.Visible = true;
+                genuddel.Visible = true;
+                Navne.Visible = true;
+                Fordel.Visible = false;
+            }
+            else
+            {
+                Glemt.Visible = false;
+                MANGLER.Visible = false;
+                genuddel.Visible = false;
+                Navne.Visible = false;
+                Fordel.Visible = true;
+            }
+        }
+
+        private void genuddel_Click(object sender, EventArgs e)
+        {
+            DialogResult gd = MessageBox.Show("Sikker på at du vil genuddele nisser?", "Genuddel", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Exclamation);
+            if (gd == DialogResult.Yes)
+            {
+                Properties.Settings.Default.Uddelt = false;
+                Properties.Settings.Default.Save();
+                Glemt.Visible = false;
+                MANGLER.Visible = false;
+                genuddel.Visible = false;
+                Navne.Visible = false;
+                Fordel.Visible = true;
+            }
+        }
+
+        private void MANGLER_Click(object sender, EventArgs e)
+        {
+            if (Navne.SelectedItem == null)
+            {
+                MessageBox.Show("Du skal vælge en person");
+            }
+            else
+            {
+                
+            }
+        }
+    }
+}
